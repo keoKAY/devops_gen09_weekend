4760daf36e87340d7b9257dbaa9f46a0565e16b9
Keo Kay <keokay888@gmail.com>
2025-07-12 07:48:12 +0000

    features: jenkin scan using sonarqube

---
+++ jenkins_lesson/sonarqube/pipeline-sonarqube.groovy
@@ -0,0 +1,39 @@
+pipeline {
+    agent any
+
+    stages {
+        stage('Clone ReactJs Code ') {
+            steps {
+                  git 'https://github.com/keoKAY/reactjs-devop8-template'
+
+            }
+        }
+
+
+        stage("Check Code Quality in Sonarqube "){
+            
+            environment {
+                scannerHome= tool 'sonarqube-scanner' 
+            }
+
+            steps{
+                withSonarQubeEnv(credentialsId: 'SONARQUBE_TOKEN', installationName: 'sonarqube-scanner') {
+                script{
+                
+                    def projectKey = 'reactjs-devops8-template' 
+                    def projectName = 'ReactjsDevOps8template'
+                    def projectVersion = '1.0.0' 
+                    sh """
+                    ${scannerHome}/bin/sonar-scanner \
+                    -Dsonar.projectKey=${projectKey} \
+                    -Dsonar.projectName="${projectName}" \
+                    -Dsonar.projectVersion=${projectVersion} \
+                     """   
+                        
+                        }
+            }
+
+            }
+        }
+    }
+}
